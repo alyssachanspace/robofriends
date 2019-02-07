ff3000398368cc14c89a3fe43aa9efd165643390
Alyssa Chan <alyssa@whiletrue.codes>
2019-02-07 21:19:21 +0800

    create actions of requesting reobots

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger'
-import './index.css';
-import App from './containers/App';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+
+import App from './containers/App';
 import { searchRobots } from './reducers';
+import './index.css';
 
 const logger = createLogger()
-const store = createStore(searchRobots, applyMiddleware(logger))
+const store = 
+  createStore(searchRobots, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
